5cfdb42991e4f8a12bf67da9729fdcae4849db8c
Mitul Shah <ubuntu@dfc2eabdf236>
2017-03-12 01:58:06 +0000

    initial commit

---
+++ pset2/initials/initials.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int main(int argc, string argv[]){
+    printf("What is your name? ");
+    string s = get_string();
+    printf("What is your name? %s/n", s);
+}
